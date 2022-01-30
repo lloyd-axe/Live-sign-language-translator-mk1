c7a94a6326edcebe66d662f514880061bd731b3d
MSI\Lloyd Acha <lloydacha@gmail.com>
2022-01-30 16:39:44 +0800

    GetDataFromPath() update

--- ASLT.py
+++ ASLT.py
@@ -1,3 +1,4 @@
+import os
 import cv2 as cv
 import numpy as np
 from keras.models import load_model
@@ -24,7 +25,7 @@ class ASLTranslator: #ASL Translator
         textColor= (0,0,255), 
         fontSize = 1):
         #internal parameters
-        sentence, sequence = [], []
+        sentence, sequences = [], []
         frame_no = 0
         
         #Start video
@@ -39,13 +40,13 @@ class ASLTranslator: #ASL Translator
             results = self.tracker.Detect(frame)
             keyPoints = self.tracker.GetKeypointsData(results, noFace = True)
             frame_no += 1
-            sequence.insert(0, keyPoints)
-            sequence = sequence[:interval]
+            sequences.insert(0, keyPoints)
+            sequences = sequences[:interval]
             
             #predict
             if frame_no >= interval:
                 frame_no = 0
-                prediction = self.model.predict(np.array(sequence))[0]
+                prediction = self.model.predict(np.array(sequences))[0]
                 if prediction[np.argmax(prediction)] >= threshold:
                     #form a sentence
                     word = self.words[np.argmax(prediction)]
@@ -74,6 +75,75 @@ class ASLTranslator: #ASL Translator
                 break
         cap.release()
         cv.destroyAllWindows()
-            
+
+    def GetDataFromPath(
+        self, 
+        data_path, 
+        saveNpy = True):
+        #each word should be on separate folders
+        label_map = {label: num for num, label in enumerate(self.words)}
+        sequences, labels = [], []
+        for word in self.words:
+            word_path = os.path.join(data_path,word)
+            print(f'Obtaining data for "{word}"...')
+            if os.path.isdir(word_path):
+                for file_name in os.listdir(word_path):
+                    file_path = os.path.join(word_path, file_name)
+                    
+                    #check if numpy file already exists
+                    npy_path = f"{file_path.split('.')[0]}.npy"
+                    vid_path = f"{file_path.split('.')[0]}_np"
+                    if os.path.isfile(npy_path): #load numpy
+                        keyPoints = np.load(npy_path)
+                        sequences.append(keyPoints)
+                        labels.append(label_map[word])
+                    elif os.path.isdir(vid_path): #load numpy for vid
+                        for npy_path in os.listdir(vid_path):
+                            npy_path = os.path.join(vid_path,npy_path)
+                            keyPoints = np.load(npy_path)
+                            sequences.append(keyPoints)
+                            labels.append(label_map[word])
+                    else: #load file
+                        if os.path.isfile(file_path):
+                            #identify file
+                            file_ext = file_name.split('.')[1]
+                            if file_ext in ['jpg', 'png', 'jpeg']: #Image file
+                                frame = cv.imread(file_path)
+                                #add to sequences
+                                keyPoints = self.tracker.GetKeypointsData(self.tracker.Detect(frame), noFace = True)
+                                sequences.append(keyPoints)
+                                labels.append(label_map[word])
+                                #save to numpy
+                                if saveNpy:
+                                    np.save(npy_path, keyPoints)
+                            elif file_ext in ['mp4', 'mkv']: #Video file
+                                cap = cv.VideoCapture(file_path)
+                                vid_frame = 0
+                                if not os.path.isdir(vid_path):
+                                    os.makedirs(vid_path)
+                                while cap.isOpened():
+                                    isTrue, frame = cap.read()
+                                    if not isTrue:
+                                        break
+                                    #add to sequences
+                                    keyPoints = self.tracker.GetKeypointsData(self.tracker.Detect(frame), noFace = True)
+                                    sequences.append(keyPoints) 
+                                    labels.append(label_map[word])
+                                    npy_path = os.path.join(vid_path,f'{vid_frame}.npy')
+                                    vid_frame += 1
+                                    if saveNpy:
+                                        np.save(npy_path, keyPoints)
+                                    
+                            else:
+                                print('File is not recognized')
+                                return None
+                        elif os.path.isdir(file_path):
+                            pass
+                        else:
+                            print(f'{file_name} does not exists!')
+            else:
+                print(f'{word_path} does not exists!')
+                return None
+        return sequences, labels      
         
         
--- tester.py
+++ tester.py
@@ -2,5 +2,16 @@ from ASLT import ASLTranslator
 
 with open('words.txt') as file:
     words = [line.rstrip() for line in file]
-aslt = ASLTranslator('model.h5', words)
-aslt.RunVideo(threshold = 0.7, draw=[False, False, True, True])
+aslt = ASLTranslator('new_model.h5', words)
+
+def StartVideo():
+    aslt.RunVideo(interval = 30, threshold = 0.7, draw=[False, False, True, True])
+
+def TrainModel():
+    sequences, labels = aslt.GetDataFromPath('test_data')
+    #create new model here
+
+###################################################################
+
+StartVideo()
+#TrainModel()
