381eb71909486f6d28fee2714430d56bdd9086cc
MSI\Lloyd Acha <lloydacha@gmail.com>
2022-01-30 13:13:42 +0800

    update v0.4 - added MediaPipeHelper

--- ASLT.py
+++ ASLT.py
@@ -1,100 +1,79 @@
 import cv2 as cv
 import numpy as np
-import mediapipe as mp
+from keras.models import load_model
+from MediaPipeHelper import Tracker
 
-class ASLT: #ASL Translator
-    def __init__ (self):
-        #keypoints, models and stuff
-        self.holistic = mp.solutions.holistic
-        self.drawing = mp.solutions.drawing_utils
+class ASLTranslator: #ASL Translator
+    def __init__ (
+        self, 
+        model, 
+        words):
+        #Required model input shape will be (x, 258)
+        self.model = load_model(model)
+        self.words = words
+        self.tracker = Tracker()
         
-    def run(self, 
-            model, 
-            actions,
-            isDraw = [True, True, True], 
-            threshold = 0, 
-            interval = 10):
-        cap = cv.VideoCapture(0)
-        sequence = []
-        sentence = []
-        if threshold == 0:
-            threshold = ((1/(len(actions))) * (len(actions) - 1))*0.9
-        with self.holistic.Holistic(
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5) as holistic_model:
-
-            i = 0 #interval count
-            while cap.isOpened():
-                isTrue, frame = cap.read()
-                if not isTrue:
-                    break
-                results = self.detect(frame, holistic_model) #get landmarks in frame
-                self.drawLandmarks(frame, results, isDraw[0], isDraw[1], isDraw[2])               
-                keyPoints = self.extractKeypoints(results)
-                sequence.insert(0, keyPoints)
-                sequence = sequence[:interval] 
-                i += 1
-                if i > interval:
-                    i = 0
-                    pred = model.predict(np.array(sequence))[0]
-
-                    #VISUALIZE
-                    if pred[np.argmax(pred)] > threshold:
-                        action = actions[np.argmax(pred)]
-                        if len(sentence) > 0:
-                            if action != sentence[-1]:
-                                sentence.append(action)
-                        else:
-                            sentence.append(action)
-
-                    if len(sentence) > 5:
-                        sentence = sentence[-5:]
-                    
-                cv.putText(frame,
-                           ' '.join(sentence),
-                           (10,50),
-                           cv.FONT_HERSHEY_SIMPLEX,
-                           1,
-                           (0, 0,255),
-                           2,
-                           cv.LINE_AA)
-                
-                cv.imshow('ASL Translate', frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    break
-            cap.release()
-            cv.destroyAllWindows()      
-    
-    def detect(self, frame, model):
-        return model.process(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
-    
-    def drawLandmarks(self,
-                      frame,
-                      results,
-                      head = True,
-                      pose = True,
-                      hands = True):
-        if head:
-            self.drawing.draw_landmarks(frame,
-                                      results.face_landmarks,
-                                      self.holistic.FACEMESH_TESSELATION,
-                                      self.drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                                      self.drawing.DrawingSpec(color=(80, 255, 121), thickness=1, circle_radius=1))
-        if pose:
-            self.drawing.draw_landmarks(frame, 
-                                      results.pose_landmarks, 
-                                      self.holistic.POSE_CONNECTIONS)
-        if hands:
-            self.drawing.draw_landmarks(frame, 
-                                      results.left_hand_landmarks, 
-                                      self.holistic.HAND_CONNECTIONS)
-            self.drawing.draw_landmarks(frame, 
-                                      results.right_hand_landmarks, 
-                                      self.holistic.HAND_CONNECTIONS)
+    def RunVideo(
+        self, 
+        vid = "",
+        draw = [True, True, True, True], 
+        threshold = 0.75, 
+        interval = 10, 
+        wordCount = 5, 
+        displayText = True, 
+        textColor= (0,0,255), 
+        fontSize = 1):
+        #internal parameters
+        sentence, sequence = [], []
+        frame_no = 0
         
-    def extractKeypoints(self,
-                          results):
-        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132*4)
-        lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-        rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-        return np.concatenate([pose, lh, rh])
+        #Start video
+        cap = cv.VideoCapture(0) if vid == "" else cv.VideoCapture(vid)
+            
+        while cap.isOpened():
+            isTrue, frame = cap.read()
+            if not isTrue:
+                break
+            
+            #get data
+            results = self.tracker.Detect(frame)
+            keyPoints = self.tracker.GetKeypointsData(results, noFace = True)
+            frame_no += 1
+            sequence.insert(0, keyPoints)
+            sequence = sequence[:interval]
+            
+            #predict
+            if frame_no >= interval:
+                frame_no = 0
+                prediction = self.model.predict(np.array(sequence))[0]
+                if prediction[np.argmax(prediction)] >= threshold:
+                    #form a sentence
+                    word = self.words[np.argmax(prediction)]
+                    if len(sentence) > 0:
+                        if word != sentence[-1]:
+                            sentence.append(word)
+                    else:
+                        sentence.append(word)
+                if len(sentence) > wordCount:
+                    sentence = sentence[-wordCount:]
+                   
+            #visualization
+            self.tracker.DrawLandmarks(frame, results, draw[0], draw[1], draw[2], draw[3])
+            if displayText:
+                cv.putText(
+                    frame, 
+                    ' '.join(sentence), 
+                    (10,50), 
+                    cv.FONT_HERSHEY_SIMPLEX, 
+                    fontSize, 
+                    textColor, 
+                    2, 
+                    cv.LINE_AA)
+            cv.imshow('Capture', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv.destroyAllWindows()
+            
+        
+        
